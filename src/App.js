ae351b04b96b50880f3502e84bd3dc58695e0706
Logan Dallalio <logan@dallalioweb.dev>
2021-08-12 05:04:49 -0500

    Section 4.20 Alert State & Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
@@ -9,7 +10,8 @@ class App extends Component {
 
   state = {
     users: [],
-    loading:false
+    loading:false,
+    alert: null
   }
 
   /* async componentDidMount() {
@@ -38,6 +40,13 @@ class App extends Component {
 
   //Clear Users from state
   clearUsers = () => this.setState({ users: [], loading:false });
+  
+  //SetAlert
+  setAlert = (msg, type) => {
+    this.setState({alert: {msg, type}});
+
+    setTimeout(() => this.setState({ alert:null}), 5000);
+  }
 
   render(){
     const { users,  loading } = this.state;
@@ -46,9 +55,12 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search searchUsers={ this.searchUsers } 
           clearUsers={this.clearUsers} 
-          showClear={ users.length > 0 ? true : false  } />
+          showClear={ users.length > 0 ? true : false  }
+          setAlert={ this.setAlert } 
+          />
 
           <Users loading={ loading } 
           users={ users } />
